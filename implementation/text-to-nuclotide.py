6e644106ee81f32369c97f5cc06554bbd49db4da
angelina-tsuboi <angelina.t1832@gmail.com>
2023-11-16 12:20:18 -0800

    feat: finish text to nucleotide

--- implementation/text-to-nuclotide.py
+++ implementation/text-to-nuclotide.py
@@ -11,22 +11,28 @@ def string_to_nuclotide_sequence(val):
     binary_string = ""
     # create a sequence of bytes put into the encoding of UTF-8
     byte_string = bytearray(val, 'utf-8')
-    print("h", byte_string)
+    
     for i in byte_string:
         # appends int to binary string after converting it to binary. This results in 8 bits total and retains the leading 0s via "08b"
-        print(format(i, '08b'))
         binary_string += str(format(i, '08b'))
-    print("hi", binary_string)
+    
+    print("Binary Representation: ", binary_string)
 
     binary_bit_list = []
 
     for i in range(0, len(binary_string), 2):
         binary_bit_list.append(binary_string[i:i+2])
 
-    print(binary_bit_list)
+    result_nucleotide_string = ""
 
+    for group in binary_bit_list:
+        result_nucleotide_string += Nucleotide_Bases.get(group)
+
+    return result_nucleotide_string
     
 
 text = input("Enter in text input:")
-string_to_nuclotide_sequence(text)
+print("Text Input: ", text)
+output = string_to_nuclotide_sequence(text)
+print("Nucleotide Representation: ", output)
 
