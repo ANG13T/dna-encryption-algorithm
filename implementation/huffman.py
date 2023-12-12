414d1fb29349124e6d0c3d2b32f374ad62b4c07c
angelina-tsuboi <angelina.t1832@gmail.com>
2023-12-12 10:41:45 -0800

    fix: huffman coding binary tree

--- implementation/huffman.py
+++ implementation/huffman.py
@@ -19,23 +19,23 @@ Procedure:
 """
 
 class Node:
-    def __init__(self, data):
+    def __init__(self, data, label = ""):
         self.left = None
         self.right = None
         self.data = data
+        self.label = label
 
     def insert(self, node):
         if self.data:
-            if node.data < self.data:
-                if self.left == None:
+            if self.right == None:
+                self.right = node
+            elif self.left == None:
+                self.left = node
+            else:
+                if node.label != "" and node.data > self.left.data:
+                    self.right = node
+                else:
                     self.left = node
-                else:
-                    self.left.insert(node)
-            elif node.data > self.data:
-                if self.right == None:
-                    self.right = Node(node)
-                else:
-                    self.right.insert(node)
         else:
             self.data = node.data
 
@@ -49,6 +49,7 @@ class Node:
 # Given Freq Dict -> Return Huffman Coding Table
 def huffman_coding(frequencies):
     print(frequencies)
+    labels = list(frequencies.keys())
     frequencies = list(frequencies.values())
 
     # init base node
@@ -57,36 +58,23 @@ def huffman_coding(frequencies):
     fre_2 = frequencies[2]
     total = fre + fre_2
     total_node = Node(total)
+    total_node.insert(Node(fre_2, labels[2]))
+    total_node.insert(Node(fre, labels[3]))
     selected_node = total_node
 
+    # construct the tree
     for i in range(3,5):
        combo = selected_node.data + frequencies[4 - i]
        new_node = Node(combo)
+       new_node.insert(Node(frequencies[4 - i], labels[4 - i]))
        new_node.insert(selected_node)
-       new_node.insert(Node(frequencies[4 - i]))
        selected_node = new_node
 
+    # selected_node will be at the top of the tree
     print(selected_node.data)
+    print(selected_node.right.data)
+    print(selected_node.left.data)
+
     
     return
 
-    # construct binary tree
-    fre = frequencies[0] + frequencies[1]
-    print()
-
-    return
-    # TODO: repeat for each frequency
-    freq_1 = frequencies[-1]
-    freq_2 = frequencies[-2]
-    freq_count = freq_1 + freq_2
-
-    # creating binary tree
-    root = Node(freq_count)
-    root.insert(freq_1)
-    root.insert(freq_2)
-
-    root.display()
-    print(freq_1, freq_2, freq_count)
-    print(root.data)
-
-    
