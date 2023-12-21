9b00355f488513b5fcfb6bcd6ea9292fa6627604
angelina-tsuboi <angelina.t1832@gmail.com>
2023-12-21 09:58:45 -0800

    feat: update content

--- implementation/research_encryption.py
+++ implementation/research_encryption.py
@@ -9,15 +9,28 @@ Convert the message into Mbin using variable length code
 P (Plain text) -> M (DNA Bases) -> MBIN variable length code for each base
 """
 
+
+"""
+- DNA Structure
+- Nucleotide (Base 4)
+- Binary (2 bit encoding)
+- Freq
+- Huffman Coding to Get Key
+- Convert nucleotide bases into cipher
+"""
+
 import conversion
 import huffman
 from collections import Counter
 
 text = "hello"
+print("Plain text: " + text)
 result_string = conversion.string_to_nucleotide_sequence(text)
-print(result_string)
+print("DNA Bases: " + result_string)
 frequencies = Counter(result_string)
+print("Frequencies")
 print(frequencies)
 huffman_codings = huffman.huffman_coding(frequencies)
+print("Huffman Codings: ",  huffman_codings)
 cipher = conversion.nucleotide_string_to_cipher(result_string, huffman_codings)
-
+print("Cipher: " + cipher)
--- research.md
+++ research.md
@@ -3,4 +3,20 @@
 - Chaos Theory 
 - Cellular Processes
 - Bioinformatics
+- Gibbs Free Energy of Protein Folding
+- Codon Profiling
+- Frequency Distribution (PDE?)
+ 
 
+ # Things to Implement
+ - Make the key into a string (A10T100C00G10)
+ - Make a web server
+ - Make a database backend 
+ - Make a file input (files, text files, videos, etc)
+ - Encrypt the files
+ - Save the files within the database 
+ - Retrieve encrypted files 
+- POC file encryption using DNA encryption techniques
+- make a cusotm EFC (Encrypted File System) + Server + DB Solution
+- have a key embedded in the header of the data
+- End-to-End Encrypted EFS Database using DNA Encryption created using Python
